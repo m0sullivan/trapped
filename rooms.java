081a275a4ff3e31679f8a572ac0bf3eea3529045
SpiceProjects <116753838+SpiceProjects@users.noreply.github.com>
2023-11-29 10:25:35 -0500

    Create rooms.java

---
+++ rooms.java
@@ -0,0 +1,16 @@
+
+public class rooms{
+    int id;
+		String name;
+		double chanceOfCreating;
+
+		private rooms(double chanceOfCreating, String name, int id) {
+			chanceOfCreating = chanceOfCreating; 
+			name = name;
+			id = id;
+		}
+}
+
+public mainRoom() {
+    
+}
