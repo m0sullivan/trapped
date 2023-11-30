bd2f8f5ab4d01f4d04c56af7acc7dc17c597becd
SpiceProjects <116753838+SpiceProjects@users.noreply.github.com>
2023-11-30 10:28:49 -0500

    Update room.java

--- room.java
+++ room.java
@@ -1,13 +1,13 @@
 
 public class Room{
     int id;
-		String name;
-		double chanceOfCreating;
+    String name;
+    double chanceOfCreating;
 
-		private Room(double chanceOfCreating, String name, int id) {
-			chanceOfCreating = chanceOfCreating; 
-			name = name;
-			id = id;
-		}
+    private Room(double chanceOfCreating, String name, int id) {
+	chanceOfCreating = chanceOfCreating; 
+	name = name;
+	id = id;
+   }
 }
 
