528f4605fef6f06c974d87ce1c2aef8b16cb7230
AntFunny2314 <152206955+AntFunny2314@users.noreply.github.com>
2023-11-30 10:51:34 -0500

    Update and rename CluesComboLocks.java to ClueComboLock.java

---
+++ ClueComboLock.java
@@ -0,0 +1,9 @@
+public class ClueComboLock {
+  String Clue; 
+  String CombinationLock;
+
+  public ClueComboLock(String Ce, String CbL) {
+    this.Ce = Clue;
+    this.CbL = CombinationLock;
+  }
+}
--- CluesComboLocks.java
+++
@@ -1,8 +0,0 @@
-public class CluesComboLocks {
-  String clues; 
-  String combinationLock;
-
-  public CluesComboLocks(clues Cs, combinationLock CbL) {
-    
-  }
-}
