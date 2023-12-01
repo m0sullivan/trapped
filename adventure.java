c5b781b781bfb7fc4e702e6b96c1b32e3a2bfdb6
SpiceProjects <116753838+SpiceProjects@users.noreply.github.com>
2023-12-01 11:42:15 -0500

    Update adventure.java

--- adventure.java
+++ adventure.java
@@ -2,26 +2,28 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class adventure {
-    ResultSet clues;
-    ResultSet combinationlock;
-    ResultSet enemies;
-    ResultSet furniture;
-    static ResultSet rooms;
+
     
-    static boolean gameRunning;
+
     public static void main(String[] args) {
-        
-        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "Keefe2012")) {
+	    ResultSet clues;
+	    ResultSet combinationlock;
+	    ResultSet enemies;
+	    ResultSet furniture;
+	    ResultSet rooms = null;        
+	    boolean gameRunning = false;  
+	    try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "Keefe2012")) {
 
             System.out.println("Database connected!");
             Statement stmt = conn.createStatement();
             stmt.executeUpdate("USE TrappedGame;");
 
-            ResultSet clues = stmt.executeQuery("SELECT * FROM clues;");
-            ResultSet combinationlock = stmt.executeQuery("SELECT * FROM combinationlock;");
-            ResultSet enemies = stmt.executeQuery("SELECT * FROM enemies;");
-            ResultSet furniture = stmt.executeQuery("SELECT * FROM furniture;");
-            ResultSet rooms = stmt.executeQuery("SELECT * FROM rooms;");
+            clues = stmt.executeQuery("SELECT * FROM clues;");
+            combinationlock = stmt.executeQuery("SELECT * FROM combinationlock;");
+            enemies = stmt.executeQuery("SELECT * FROM enemies;");
+            furniture = stmt.executeQuery("SELECT * FROM furniture;");
+            rooms = stmt.executeQuery("SELECT * FROM rooms;");
+            System.out.println("You are in: " + rooms.getString(1));
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -35,12 +37,8 @@ public class adventure {
             System.out.println("You are trapped inside of a house, and you need to find a way out.");
             
         }
-        try {
-			System.out.println("You are in: " + rooms.getString(1));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        
+
     }
 
 }
