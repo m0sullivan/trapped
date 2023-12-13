d788b2b465c706acdec2e5e3589b35a8dd41ccff
lanasoares0 <152314142+lanasoares0@users.noreply.github.com>
2023-12-13 12:37:24 -0500

    woah

--- Adventure.java
+++ Adventure.java
@@ -26,18 +26,13 @@ public class Adventure {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    System.out.println("            Welcome to Trapped");
 	    System.out.println("--------------------------------------------");
 	    System.out.println("Press ENTER to begin");
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    input = reader.readLine();
 	    gameRunning = true;
-	    System.out.println("--------------------------------------------");
-	    System.out.println("Controls:");
-	    System.out.println("C anytime to show controls");
-	    System.out.println("Q to Quit anytime");
-	    System.out.println("H to Hide when enemy appears");
-	    System.out.println("DOOR to open and go through a door");
+	    controls();
 	    System.out.println("--------------------------------------------");
 	    System.out.println("You are trapped inside of a house, and you need to find a way out.");
 	    
@@ -50,17 +45,18 @@ public class Adventure {
 	    while(gameRunning) {
 	    	System.out.println("--------------------------------------------");
 	    	System.out.println("What would you like to do?");
+	    	options();
 	    	input = reader.readLine();
-	        if (input.equals("Q")) {
+	        if (input.equals("QUIT")) {
 	        	System.out.println("--------------------------------------------");
-	        	System.out.println("You Quit! Please type START to start again.");
+	        	System.out.println("You Quit! Please type START to start again, or press ENTER to close game.");
 	        	input = reader.readLine();
 	        	if (input.equals("START")) {
 	        		
 	        	}
-	        	else {gameRunning = false;}
+	        	System.exit(0);
 	        }
-	        else if (input.equals("C")) {
+	        else if (input.equals("CONTROLS")) {
 	        	controls();
 	        }
 	        
@@ -70,15 +66,13 @@ public class Adventure {
     }
     public static void controls() {
     	System.out.println("--------------------------------------------");
-	    System.out.println("Controls:");
-	    System.out.println("C anytime to show controls");
-	    System.out.println("Q to Quit anytime");
-	    System.out.println("H to Hide when enemy appears");
+	    System.out.println("CONTROLS:");
+	    System.out.println("Type CONTROLS anytime to show controls");
+	    System.out.println("Type QUIT to Quit anytime");
+	    System.out.println("Type HIDE to Hide when enemy appears");
 	    System.out.println("DOOR to open and go through a door");
-	    System.out.println("OPTIONS to show available options");
     }
     public static void options() {
-    	System.out.println("--------------------------------------------");
     	System.out.println("1. ");
     	System.out.println("2. ");
     	System.out.println("3. ");
