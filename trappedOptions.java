b121bf5368dac5d50585c0bda34cf1ff0127af70
lanasoares0 <152314142+lanasoares0@users.noreply.github.com>
2023-12-14 10:40:11 -0500

    Update trappedOptions.java

--- trappedOptions.java
+++ trappedOptions.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 
 public class trappedOptions extends Adventure{
 	Connection conn;
+	Random rnd = new Random();
 	Scanner myScan = new Scanner(System.in);
 	String userInput = "";
 	public void mainOptions(Connection C) throws SQLException {
 		conn = C;
 	    ResultSet rooms = null;
         Statement stmt = conn.createStatement();
-        Random rnd = new Random();
         System.out.println("1. Go to door1");
     	System.out.println("2. Go to door2");
     	System.out.println("3. Try to open Main door");
@@ -22,15 +22,15 @@ public class trappedOptions extends Adventure{
     	userInput = myScan.nextLine();
     	if (userInput.equals("1"))
 		{
-			
+    		System.out.println("Door1 is unlocked. Do you want to go to the office?");
 		}
 		else if (userInput.equals("2"))
 		{
-			
+			System.out.println("Door2 is unlocked. Do you want to go to the bedroom?");
 		}
 		else if (userInput.equals("3"))
 		{
-		
+			System.out.println("Main door is locked");
 		}
 		else if (userInput.equals("4"))
 		{
@@ -42,7 +42,6 @@ public class trappedOptions extends Adventure{
 		conn = C;
 	    ResultSet rooms = null;
         Statement stmt = conn.createStatement();
-        Random rnd = new Random();
         System.out.println("1. Try to open the door");
     	System.out.println("2. Look inside the file cabinet");
     	System.out.println("3. Look for something on desk");
@@ -50,26 +49,25 @@ public class trappedOptions extends Adventure{
     	userInput = myScan.nextLine();
     	if (userInput.equals("1"))
 		{
-			
+    		System.out.println("The door is locked");
 		}
 		else if (userInput.equals("2"))
 		{
-			
+			System.out.println("There is nothing inside the file cabinet");
 		}
 		else if (userInput.equals("3"))
 		{
-		
+			System.out.println("There is a letter on top of the table. I should look at it");
 		}
 		else if (userInput.equals("4"))
 		{
-		
+			System.out.println("");
 		}
 	}
 	public void bedroomOptions(Connection C) throws SQLException {
 		conn = C;
 	    ResultSet rooms = null;
         Statement stmt = conn.createStatement();
-        Random rnd = new Random();
 		System.out.println("1. Try to open the door");
     	System.out.println("2. Lay in bed");
     	System.out.println("3. Look inside the closet");
@@ -77,26 +75,25 @@ public class trappedOptions extends Adventure{
     	userInput = myScan.nextLine();
     	if (userInput.equals("1"))
 		{
-			
+    		System.out.println("");
 		}
 		else if (userInput.equals("2"))
 		{
-			
+			System.out.println("It's as comfortable as it looks");
 		}
 		else if (userInput.equals("3"))
 		{
-		
+			System.out.println("");
 		}
 		else if (userInput.equals("4"))
 		{
-		
+			System.out.println("");
 		}
 	}
 	public void libraryOptions(Connection C) throws SQLException {
 		conn = C;
 	    ResultSet rooms = null;
         Statement stmt = conn.createStatement();
-        Random rnd = new Random();
 		System.out.println("1. Go to Door");
     	System.out.println("2. ");
     	System.out.println("3. ");
@@ -104,19 +101,19 @@ public class trappedOptions extends Adventure{
     	userInput = myScan.nextLine();
     	if (userInput.equals("1"))
 		{
-			
+    		System.out.println("");
 		}
 		else if (userInput.equals("2"))
 		{
-			
+			System.out.println("");
 		}
 		else if (userInput.equals("3"))
 		{
-		
+			System.out.println("");
 		}
 		else if (userInput.equals("4"))
 		{
-		
+			System.out.println("");
 		}
 	}
 }
